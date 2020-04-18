b9323d9b4d72df39a120e09054ad9b133764149b
Shu Urata <atarubift@gmail.com>
2020-04-18 15:25:07 +0900

    delete HTML constructure

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | ARCH-app"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | ARCH-app"
   end
 
 end
