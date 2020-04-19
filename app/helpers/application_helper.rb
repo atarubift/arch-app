ca05b06235997f107c3ef80990f0add926ee38a2
Shu Urata <atarubift@gmail.com>
2020-04-19 11:10:27 +0900

    Definition full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+    # ページごとのタイトルを返す
+    def full_title(page_title = '')
+        base_title = "ACNH-checkbox"
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,13 +4,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "| ARCH-app"
+    assert_select "title", "ACNH-checkbox"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | ARCH-app"
+    assert_select "title", "Help | ACNH-checkbox"
   end
 
 end
