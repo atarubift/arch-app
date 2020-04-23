a662cbf2d4ec67cad7a3e5ce502ff3f662ddf25c
Shu Urata <atarubift@gmail.com>
2020-04-23 16:09:45 +0900

    パスワードのテストを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_065849) do
+ActiveRecord::Schema.define(version: 2020_04_23_070327) do
 
   create_table "diys", force: :cascade do |t|
     t.string "name"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2020_04_23_065849) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
