02c140144b3d9d0dac2cb6d8d8c2f610642e3f00
Shu Urata <atarubift@gmail.com>
2020-04-25 14:20:36 +0900

    remember機構の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_070327) do
+ActiveRecord::Schema.define(version: 2020_04_25_041841) do
 
   create_table "diys", force: :cascade do |t|
     t.string "name"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 2020_04_23_070327) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
